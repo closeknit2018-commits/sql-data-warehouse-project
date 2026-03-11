c4e81571daad236dfbc9c22a101b12f10c516963
closeknit2018-commits <closeknit2018@gmail.com>
2026-03-11 10:41:04 +0100

    Add files via upload

---
+++ Build_The_Bronze_Layer_Data_Ingestion.sql
@@ -0,0 +1,81 @@
+	DROP DATABASE IF EXISTS DataWarehouse
+	GO 
+
+	CREATE DATABASE DataWarehouse
+	GO
+
+	USE DataWarehouse
+	
+
+DROP TABLE IF EXISTS bronze_Customer_info
+
+CREATE TABLE bronze_Customer_info (
+	CustomerName NVARCHAR(50)
+	,CustomerPhNo NVARCHAR (50)
+	,CustomerAddress NVARCHAR(120)
+	,Password NVARCHAR(50)
+)
+
+
+DROP TABLE IF EXISTS bronze_Branch_info
+
+CREATE TABLE bronze_Branch_info (
+	Brand_Id TINYINT
+	,Brand_Name NVARCHAR(12)
+)
+
+
+DROP TABLE IF EXISTS bronze_BranchProduct_info
+
+CREATE TABLE bronze_BranchProduct_info (
+	Brands_Brand_id TINYINT
+	,Products_ProductID NVARCHAR(50)
+)
+
+
+DROP TABLE IF EXISTS bronze_Product_info
+
+CREATE TABLE bronze_Product_info (
+	ProductID NVARCHAR (50)
+	,ProductName NVARCHAR(500)
+	,ProductStock NVARCHAR(20)
+	,Price NVARCHAR(120)
+	,Discount NVARCHAR(20)
+	,Category_CategoryID NVARCHAR(20)
+)
+
+
+DROP TABLE IF EXISTS bronze_Cashier_info
+
+CREATE TABLE bronze_Cashier_info (
+	CashierID TINYINT
+	,CashierName NVARCHAR(20)
+	,CashierPhoneNo NVARCHAR (50)
+	,CashierAddress NVARCHAR(50)
+)
+
+
+DROP TABLE IF EXISTS bronze_Categories_info
+
+CREATE TABLE bronze_Categories_info (
+	CategoryId TINYINT
+	,Category NVARCHAR(50)
+	,SubCategory NVARCHAR(50)
+)
+
+
+DROP TABLE IF EXISTS bronze_Transactions_details
+
+CREATE TABLE bronze_Transactions_details (
+	Transaction_date DATE
+	,Quantity INT
+	,Subtotal NVARCHAR(250)
+	,Taxes NVARCHAR(250)	
+	,DiscountPrice NVARCHAR(250)
+	,TotalAmount NVARCHAR(250)
+	,Payment_Method NVARCHAR(250)
+	,Products_ProductID NVARCHAR(250)
+	,CustomerCart_CustomerID INT
+	,Cashier_CashierID INT
+)
+SELECT * FROM bronze_Transactions_details
